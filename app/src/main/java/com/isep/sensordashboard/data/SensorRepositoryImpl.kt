9b0136c65008567aabfafdedb8239513ed93f98b
Kelyan Taverny <taverny.k@gmail.com>
2025-10-27 20:54:03 +0100

    feat: SensorRepository API + implementation

---
+++ app/src/main/java/com/isep/sensordashboard/data/SensorRepositoryImpl.kt
@@ -0,0 +1,15 @@
+﻿package com.isep.sensordashboard.data
+
+import com.isep.sensordashboard.model.SensorReading
+import com.isep.sensordashboard.model.SensorType
+import com.isep.sensordashboard.sensors.AndroidSensorDataSource
+import com.isep.sensordashboard.sensors.SamplingRate
+import kotlinx.coroutines.flow.Flow
+
+class SensorRepositoryImpl(
+    private val ds: AndroidSensorDataSource
+) : SensorRepository {
+    override fun availableSensors(): List<SensorType> = ds.available()
+    override fun readings(type: SensorType, rate: SamplingRate): Flow<SensorReading> =
+        ds.stream(type, rate)
+}
